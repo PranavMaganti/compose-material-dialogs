8604458f81d1a2e19538ac8e771e2657f236e44b
Pranav Maganti <pranav.maganti@gmail.com>
2020-07-16 11:55:54 +0100

    Change button layout to allow stacking

--- app/src/main/java/com/vanpra/composematerialdialogs/MainActivity.kt
+++ app/src/main/java/com/vanpra/composematerialdialogs/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.vanpra.composematerialdialogs
 
-import android.graphics.Color.*
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.state
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Text
-import androidx.ui.graphics.Color
-import androidx.ui.material.TextButton
-import com.afollestad.materialdialogs.color.colorChooser
-//import com.afollestad.materialdialogs.color.ColorPalette
 import com.vanpra.composematerialdialogs.ui.ComposeMaterialDialogsTheme
-import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeMaterialDialogsTheme {
+                DialogAndShowButton(buttonText = "Basic Dialog With Buttons") {
+                    title("Use Location Services?")
+                    message("Let us help apps determine location. This means sending anonymous location data to us, even when no apps are running")
+                    buttons {
+                        negativeButton("Disagree")
+                        positiveButton("Agree")
+                    }
+                }
             }
         }
     }
