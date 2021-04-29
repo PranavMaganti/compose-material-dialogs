26606386f81871cc71ddac19e68ffd06e01df836
Pranav Maganti <pranav.maganti@gmail.com>
2021-04-29 19:48:01 +0100

    Bump shot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.14.2")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.30")
-        classpath("com.karumi:shot:5.10.3")
+        classpath("com.karumi:shot:5.10.4")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
