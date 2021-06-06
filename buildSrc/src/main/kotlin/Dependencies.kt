7fd4b6ac823e7f3234a8eab41b9a9b701b4d0c19
Pranav Maganti <pranav.maganti@gmail.com>
2021-06-06 14:04:43 +0100

    Update compose dependancy to 1.0.0-beta08

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ object Dependencies {
     const val material = "com.google.android.material:material:1.4.0-beta01"
 
     object ComposeMaterialDialogs {
-        const val version = "0.4.0"
+        const val version = "0.4.1"
 
         const val core = "io.github.vanpra.compose-material-dialogs:core:$version"
         const val datetime = "io.github.vanpra.compose-material-dialogs:datetime:$version"
@@ -14,12 +14,12 @@ object Dependencies {
     }
 
     object Accompanist {
-        private const val version = "0.10.0"
+        private const val version = "0.11.1"
         const val pager = "com.google.accompanist:accompanist-pager:$version"
     }
 
     object Kotlin {
-        private const val version = "1.4.32"
+        private const val version = "1.5.10"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     }
@@ -27,11 +27,11 @@ object Dependencies {
     object AndroidX {
         const val appcompat = "androidx.appcompat:appcompat:1.4.0-alpha01"
         const val coreKtx = "androidx.core:core-ktx:1.6.0-beta01"
-        const val composeActivity = "androidx.activity:activity-compose:1.3.0-alpha08"
-        const val composeNav = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+        const val composeActivity = "androidx.activity:activity-compose:1.3.0-beta01"
+        const val composeNav = "androidx.navigation:navigation-compose:2.4.0-alpha02"
 
         object Compose {
-            const val version = "1.0.0-beta07"
+            const val version = "1.0.0-beta08"
 
             const val ui = "androidx.compose.ui:ui:$version"
             const val material = "androidx.compose.material:material:$version"
