b65894ab27812690ff7d3948a8102e859a63d805
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2022-03-02 08:40:38 +0100

    Use java.time instead of Calendar and DateUtils

--- datetime/src/main/java/com/vanpra/composematerialdialogs/datetime/date/DatePickerState.kt
+++ datetime/src/main/java/com/vanpra/composematerialdialogs/datetime/date/DatePickerState.kt
@@ -1,14 +1,12 @@
 package com.vanpra.composematerialdialogs.datetime.date
 
-import android.text.format.DateUtils
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.format.TextStyle
 import java.time.temporal.WeekFields
-import java.util.Calendar
 import java.util.Locale
 
 internal class DatePickerState(
@@ -24,19 +22,9 @@ internal class DatePickerState(
 
         private val locale = Locale.getDefault()
 
-        private val isoDayOfWeekToCalendarDayOfWeek = mapOf(
-            DayOfWeek.MONDAY to Calendar.MONDAY,
-            DayOfWeek.TUESDAY to Calendar.TUESDAY,
-            DayOfWeek.WEDNESDAY to Calendar.WEDNESDAY,
-            DayOfWeek.THURSDAY to Calendar.THURSDAY,
-            DayOfWeek.FRIDAY to Calendar.FRIDAY,
-            DayOfWeek.SATURDAY to Calendar.SATURDAY,
-            DayOfWeek.SUNDAY to Calendar.SUNDAY
-        )
-
         val dayHeaders = WeekFields.of(locale).firstDayOfWeek.let { firstDayOfWeek ->
             (0L until 7L).map {
-                DateUtils.getDayOfWeekString(isoDayOfWeekToCalendarDayOfWeek[firstDayOfWeek.plus(it)] ?: 0, DateUtils.LENGTH_SHORTEST)
+                firstDayOfWeek.plus(it).getDisplayName(TextStyle.NARROW, locale)
             }
         }
     }
