f5c3a7e419828b7f5510130b8126632d7ad5c0b8
Pranav Maganti <pranav.maganti@gmail.com>
2022-05-16 19:03:06 +0100

    Add state parameters to list component

--- app/src/main/java/com/vanpra/composematerialdialogdemos/demos/ListDialog.kt
+++ app/src/main/java/com/vanpra/composematerialdialogdemos/demos/ListDialog.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
